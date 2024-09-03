9fe8178ceec676dd3bb23f2fa1862ed385e40e72
choudharymanisha <choudharymanisha38566@gmail.com>
2024-09-03 13:11:47 +0530

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollNo, name ,marks) values (101, 'Manisha', 78);
+insert into student (rollNo, name ,marks) values (102, 'Anika', 79);
+insert into student (rollNo, name ,marks) values (103,'Ronit' ,80);
